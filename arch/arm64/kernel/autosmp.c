0e5d5397ff22e56ddbb480b002f5bff9502a76e4
sandatjepil <masamik2222@gmail.com>
2024-01-26 16:23:45 +0700

    autosmp: enable by default

--- arch/arm64/kernel/autosmp.c
+++ arch/arm64/kernel/autosmp.c
@@ -33,6 +33,8 @@
 #include <linux/fb.h>
 
 #define ASMP_TAG "AutoSMP: "
+#define ASMP_ENABLE true
+
 
 struct asmp_load_data {
     u64 prev_cpu_idle;
@@ -50,6 +52,7 @@ static struct notifier_block asmp_nb;
 static bool started = false;
 
 static struct asmp_param_struct {
+    bool enabled;
     unsigned int delay;
     bool scroff_single_core;
     unsigned int max_cpus_bc;
@@ -63,7 +66,8 @@ static struct asmp_param_struct {
     unsigned int cycle_up;
     unsigned int cycle_down;
 } asmp_param = {
-    .delay = 100, // looks like .delay parameter 100 is work efficient for old device, change it to 1 ms
+    .enabled = ASMP_ENABLE,
+    .delay = 100,
     .scroff_single_core = true,
     .max_cpus_bc = 4, /* Max cpu Big cluster ! */
     .max_cpus_lc = 4, /* Max cpu Little cluster ! */
@@ -79,7 +83,6 @@ static struct asmp_param_struct {
 
 static unsigned int cycle = 0, delay0 = 0;
 static unsigned long delay_jif = 0;
-int asmp_enabled __read_mostly = 1;
 
 static void asmp_online_cpus(unsigned int cpu)
 {
@@ -372,7 +375,7 @@ static int __ref asmp_start(void)
 
 err_out:
 
-    asmp_enabled = 0;
+    asmp_param.enabled = false;
     return ret;
 }
 
@@ -407,7 +410,7 @@ static int set_enabled(const char *val,
     int ret;
 
     ret = param_set_bool(val, kp);
-    if (asmp_enabled) {
+    if (asmp_param.enabled) {
 
         asmp_start();
 
@@ -424,7 +427,7 @@ static struct kernel_param_ops module_ops = {
     .get = param_get_bool,
 };
 
-module_param_cb(enabled, &module_ops, &asmp_enabled, 0644);
+module_param_cb(enabled, &module_ops, &asmp_param.enabled, 0644);
 MODULE_PARM_DESC(enabled, "hotplug/unplug cpu cores based on cpu load");
 
 /***************************** SYSFS START *****************************/
@@ -602,6 +605,9 @@ static int __init asmp_init(void) {
         pr_warn(ASMP_TAG"ERROR, create sysfs kobj");
 
     pr_info(ASMP_TAG"initialized\n");
+    
+    if (asmp_param.enabled)
+        asmp_start();
 
     return 0;
 }
