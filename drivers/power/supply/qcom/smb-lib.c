77befe88e459a45fd687e1a541c4368c61045633
sandatjepil <masamik2222@gmail.com>
2024-03-04 02:14:06 +0700

    power: smb-lib: manipulate ASUS JEITA Thermal value
    always report 35 degC to force fast charging.
    when value reach 47.5 degC, report actual value.

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -3410,7 +3410,12 @@ int asus_get_prop_batt_temp(struct smb_charger *chg)
 	rc = smblib_get_prop_from_bms(chg, POWER_SUPPLY_PROP_TEMP,
 					&temp_val);
 
-	return temp_val.intval;
+	if (temp_val.intval && temp_val.intval < 475) {
+		temp_val.intval = 350;
+		return temp_val.intval;
+	} else {
+		return temp_val.intval;
+	}
 }
 
 int asus_get_prop_batt_volt(struct smb_charger *chg)
