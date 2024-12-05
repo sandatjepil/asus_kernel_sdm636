0a76dc66dbeac814917650bf3623e96cb8c5ba9d
Agus Suristyono <sandatjepil@proton.me>
2024-12-05 22:04:14 +0700

    fs: guard changes with CONFIG_KSU
    in case you want to disable it and don't want these holes exposed.
    c0a10c490d18107239e198649a892cbd189b48cf 5aaa1eb484991a8ff2b496641a76ea00c16cef16
    
    Signed-off-by: Agus Suristyono <sandatjepil@proton.me>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1674,6 +1674,7 @@ static inline bool may_mount(void)
 	return ns_capable(current->nsproxy->mnt_ns->user_ns, CAP_SYS_ADMIN);
 }
 
+#ifdef CONFIG_KSU
 static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
@@ -1707,6 +1708,7 @@ int path_umount(struct path *path, int flags)
 	mntput_no_expire(mnt);
 	return ret;
 }
+#endif
 
 /*
  * Now umount can handle mount points as well as block devices.
